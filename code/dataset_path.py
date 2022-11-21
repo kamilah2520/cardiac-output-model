6b6c18ade59a8cad582bc322d9f7522c5ce717fe
kamilah2520 <kamy0225@hotmail.com>
2022-11-20 22:29:31 -0500

    Data Review

--- code/dataset_path.py
+++ code/dataset_path.py
@@ -1,3 +1,3 @@
 ## Defining "argparse" parameters
-mimic_iv_path = '../mimic-iv-ed/mimic-iv-ed-2.0'
-output_path = '/Users/jin/Desktop/work_local/mimic4/Output_270922'
+mimic_iv_path = '../mimic-iv-ed/mimic-iv-ed-2.0/2.0/ed'
+output_path = './output'
